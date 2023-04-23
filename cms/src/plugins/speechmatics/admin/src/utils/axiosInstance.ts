6402d7c86996a34a741b2b9c79ffe9d0aaa89379
Tim De Paepe <tim.depaepe@gmail.com>
2023-04-23 09:34:06 +0200

    Bumped Strapi to v4.9.2

--- cms/src/plugins/speechmatics/admin/src/utils/axiosInstance.ts
+++ cms/src/plugins/speechmatics/admin/src/utils/axiosInstance.ts
@@ -11,11 +11,14 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   async (config) => {
-    config.headers = {
+    const newHeaders = {
+      ...config.headers,
       Authorization: `Bearer ${auth.getToken()}`,
       Accept: 'application/json',
       'Content-Type': 'application/json',
     };
+
+    config.headers = newHeaders;
 
     return config;
   },
